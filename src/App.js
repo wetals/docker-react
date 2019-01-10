006c120a354b5d6dd91f1f75a0e04458faa0e3d6
Damola Adeagbo <wetals@hotmail.co.uk>
2019-01-10 14:49:55 +0000

    text update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            New React Project
+            Hello world branch
           </p>
         </header>
       </div>
